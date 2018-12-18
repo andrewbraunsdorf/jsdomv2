9977c385b442155d55a52d019def72d79fa5da04
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-18 00:57:47 +0000

    used Array.isArray, Array.from, Array.from(titles.forEach

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const titles = document.getElementsByClassName('title');
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
+Array.from(titles).forEach(function(item){
+	console.log(item);
 });

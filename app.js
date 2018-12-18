c179459f8ad3690ca94020eccae33f1cea9046a5
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-18 23:39:51 +0000

    added new features node type is, banner.nodeType, node name is, banner.nodeName, child nodes, banner.hasChildNodes; returns true or false, and clonedBanner banner.cloneNodetrue which also returns true or false true returns the dropdown while false just returns the statement

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-var books = document.querySelectorAll('#book-list li .name');
-console.log(books);
+const banner = document.querySelector("#page-banner");
 
-var books2 = document.getElementsByClassName('name');
-console.log(books2);
+console.log("#page-banner node type is:", banner.nodeType);
+console.log("#page-banner node name is:", banner.nodeName);
+console.log("#page-banner has child nodes:", banner.hasChildNodes());
 
-Array.from(books).forEach(function(book){
-	book.textContent += ' (book title)';
-});
-
-const bookList = document.querySelector("#book-list");
-//bookList.innerHTML = "<h2>Books and more books...";
-bookList.innerHTML += "<p>This is how you add HTML</p>";
-
-
-
-
-
-
-
-
-
-
-//console.log(book.textContent);
-//bookList.innerHTML = "<h2>Books and more books...";
-
-
-
-
-// const books = document.querySelectorAll('#book-list li .name');
-
-// Array.from(books).forEach(function(book){
-//   book.textContent += ' (Book title)';
-// });
-
-// const bookList = document.querySelector('#book-list');
-// bookList.innerHTML = '<h2>Books and more books...</h2>';
-// bookList.innerHTML += '<p>This is how you add HTML content</p>';
+const clonedBanner =banner.cloneNode(true);
+console.log(clonedBanner);

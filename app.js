b3fead74a2948dc518694f0753ff7f45e542101a
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-19 00:46:24 +0000

    added new features .nextSibling, .nextElementSibling, bookList.previousSibling, bookList.previousElementSibling, and .previousElementSibling.querySelector(p).innerHTML +=

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const bookList = document.querySelector("#book-list");
 
-console.log("the parentnote is:", bookList.parentNode);
-console.log("the parentnote is:", bookList.parentElement.parentElement);
+console.log("#book-list next sibling is", bookList.nextSibling);
+console.log("#book-list next sibling is", bookList.nextElementSibling);
 
-console.log(bookList.children);
+console.log("#book-list previous sibling is", bookList.previousSibling);
+console.log("#book-list previous sibling is", bookList.previousElementSibling);
+
+
+bookList.previousElementSibling.querySelector("p").innerHTML += "<br/> Too cool for everyone else!"
+
+
+
+
+
+
+
+// const bookList = document.querySelector('#book-list');
+
+// console.log('#book-list next sibling:', bookList.nextSibling);
+// console.log('#book-list next element sibling:', bookList.nextElementSibling);
+// console.log('#book-list previous sibling:', bookList.previousSibling);
+// console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+
+// bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';

581cbec90265b34dd72345eb61eb098d73a4e599
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-20 02:45:35 +0000

    learned how to set attributes

--- app.js
+++ app.js
@@ -14,72 +14,23 @@ const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
 
-const value = addForm.querySelector('input[type="text"]').value;
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const bookName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
 
-//   // create elements
-      const li = document.createElement("li");
-   const bookName = document.createElement("span");
-   const deleteBtn = document.createElement("span"); 
-   
-   //add content
-   deleteBtn.textContent = "delete";
-   bookName.textContent = value;
-   
-   //add classes
-   bookName.classList.add("name");
-   deleteBtn.classList.add("delete");
-   
-   // append to document
-   li.appendChild(bookName);
-   li.appendChild(deleteBtn);
-   list.appendChild(li);
+  // add text content
+  bookName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to DOM
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const list = document.querySelector('#book-list ul');
-// const forms = document.forms;
-
-// // delete books
-// list.addEventListener('click', (e) => {
-//   if(e.target.className == 'delete'){
-//     const li = e.target.parentElement;
-//     li.parentNode.removeChild(li);
-//   }
-// });
-
-// // add books
-// const addForm = forms['add-book'];
-// addForm.addEventListener('submit', function(e){
-//   e.preventDefault();
-
-//   // create elements
-//   const value = addForm.querySelector('input[type="text"]').value;
-//   const li = document.createElement('li');
-//   const bookName = document.createElement('span');
-//   const deleteBtn = document.createElement('span');
-
-//   // add text content
-//   bookName.textContent = value;
-//   deleteBtn.textContent = 'delete';
-
-//   // add classes
-//   bookName.classList.add('name');
-//   deleteBtn.classList.add('delete');
-
-//   // append to DOM
-//   li.appendChild(bookName);
-//   li.appendChild(deleteBtn);
-//   list.appendChild(li);
-// });
+var book = document.querySelector("li:first-child")

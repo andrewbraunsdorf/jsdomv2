07aad2baaa739515ea8b6bc4adf6ae9f55352594
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-20 03:05:58 +0000

    allow the check box to show and hide.  Reacting to change event on form field, to a change event.  document.querySelector(#hide)document.querySelector(#hide)list.style.display

--- app.js
+++ app.js
@@ -33,4 +33,14 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
-var book = document.querySelector("li:first-child")
+
+//hide books
+const hideBox =document.querySelector("#hide");
+hideBox.addEventListener("change", function(e){
+  if(hideBox.checked){
+   list.style.display = "none"; 
+  } else{
+    list.style.display = "initial";
+  }
+
+});

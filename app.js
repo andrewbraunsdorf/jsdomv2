b50f42d27056b513fb7239a3aae98e373a7c28cf
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-20 01:15:34 +0000

    attached a submit listener to a form addForm.addEventListener(submit, function, prevent a default action e.preventDefault();, and get a value to an input field addForm.querySelector(input[type=text]).value

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const list = document.querySelector("#book-list ul");
+const list = document.querySelector('#book-list ul');
 
-//delete books
-list.addEventListener("click", function(e){
-  if(e.target.className == "delete"){
+// delete books
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    list.removeChild(li);
+    li.parentNode.removeChild(li);
   }
+});
+
+//add book-list
+const addForm = document.forms["add-book"];
+
+addForm.addEventListener("submit", function(e){
+  e.preventDefault();
+  const value = addForm.querySelector("input[type='text']").value;
+  console.log(value);
 });

9a69d67d2fd92179df155c8d23f96e1c377565cd
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2018-12-20 00:46:14 +0000

    created .addEventListener(click, function(e), using if statement e.target.className == delete, and li = e.target.parentElement;

--- app.js
+++ app.js
@@ -1,54 +1,9 @@
-var btns = document.querySelectorAll("#book-list .delete");
+const list = document.querySelector("#book-list ul");
 
-Array.from(btns).forEach(function(btn){
-  btn.addEventListener("click", function(e){
-    
+//delete books
+list.addEventListener("click", function(e){
+  if(e.target.className == "delete"){
     const li = e.target.parentElement;
-    
-    li.parentNode.removeChild(li);
-  });
+    list.removeChild(li);
+  }
 });
-
-const link = document.querySelector("#page-banner a");
-
-link.addEventListener("click", function(e){
-  e.preventDefault();
-  console.log("navigation to", e.target.textContent, "was prevented");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const listItems = document.querySelectorAll('#book-list ul li');
-
-// Array.from(listItems).forEach(function(item){
-//   item.addEventListener('click', (e) => {
-
-//     const li = e.target.parentElement;
-//     console.log('child element to remove:', li);
-//     console.log('parent element to remove child from:', li.parentElement);
-//     li.parentNode.removeChild(li);
-
-//   });
-// });
-
-// // prevent default behaviour
-
-// const link = document.querySelector('#page-banner a');
-
-// link.addEventListener('click', function(e){
-//   e.preventDefault();
-//   console.log('Navigation to', e.target.textContent, 'was prevented');
-// });
